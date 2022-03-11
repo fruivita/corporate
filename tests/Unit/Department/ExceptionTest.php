452ce18150ba9c4356ad2fc9c7567ac813427d54
fcno <fcno@users.noreply.github.com>
2022-03-11 06:15:49 +0000

    style: força padrão de código com php-cs-fixer

--- tests/Unit/Department/ExceptionTest.php
+++ tests/Unit/Department/ExceptionTest.php
@@ -21,12 +21,12 @@ test('lança exceção ao tentar cadastrar lotação com campo inválido', funct
         fn () => Department::factory()->create([$field => $value])
     )->toThrow(QueryException::class, $message);
 })->with([
-    ['id',    'texto',           'Incorrect integer value'],  //valor não conversível em inteiro
-    ['id',    null,              'cannot be null'],           //campo obrigatório
-    ['name',  Str::random(256),  'Data too long for column'], //campo aceita no máximo 255 caracteres
-    ['name',  null,              'cannot be null'],           //campo obrigatório
-    ['acronym', Str::random(51), 'Data too long for column'], //campo aceita no máximo 50 caracteres
-    ['acronym', null,            'cannot be null'],           //campo obrigatório
+    ['id',    'texto',           'Incorrect integer value'],  // valor não conversível em inteiro
+    ['id',    null,              'cannot be null'],           // campo obrigatório
+    ['name',  Str::random(256),  'Data too long for column'], // campo aceita no máximo 255 caracteres
+    ['name',  null,              'cannot be null'],           // campo obrigatório
+    ['acronym', Str::random(51), 'Data too long for column'], // campo aceita no máximo 50 caracteres
+    ['acronym', null,            'cannot be null'],           // campo obrigatório
 ]);
 
 test('lança exceção ao tentar definir relacionamento inválido, isto é, com lotação pai inexistente', function () {
